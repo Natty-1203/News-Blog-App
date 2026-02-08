5f42fea5da51325a4c2a04a276462cc844c4ff62
Natty-1203 <natatanatata79@gmail.com>
2026-02-08 13:08:34 +0300

    adding responsiveness

--- src/components/calander.jsx
+++ src/components/calander.jsx
@@ -4,7 +4,7 @@ import "./calander.css";
 const Calender = () => {
   const [currentMonth, setCurrentMonth] = React.useState(new Date().getMonth());
   const [currentYear, setCurrentYear] = React.useState(
-    new Date().getFullYear()
+    new Date().getFullYear(),
   );
   const daysOfWeak = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const monthNames = [
@@ -24,57 +24,63 @@ const Calender = () => {
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   const firstDayInMonth = new Date(currentYear, currentMonth, 1).getDay();
   return (
-    <div className="calender">
-      <div className="nav-date">
-        <h2 className="month">{monthNames[currentMonth]}</h2>
-        <h2 className="year">{currentYear}</h2>
-        <button className="buttons">
-          <i
-            className="fa-solid fa-angle-left"
-            onClick={() => {
-              setCurrentMonth((prevMon) => (prevMon === 0 ? 11 : prevMon - 1));
-              setCurrentYear((prevYear) =>
-                currentMonth === 0 ? prevYear - 1 : prevYear
-              );
-            }}
-          ></i>
-          <i
-            className="fa-solid fa-angle-right"
-            onClick={() => {
-              setCurrentMonth((prevMon) => (prevMon === 11 ? 1 : prevMon + 1));
-              setCurrentYear((prevYear) =>
-                currentMonth === 11 ? prevYear + 1 : prevYear
-              );
-            }}
-          ></i>
-        </button>
-      </div>
+    <div id="calanderContainer" >
+      <div className="calender">
+        <div className="nav-date">
+          <h2 className="month">{monthNames[currentMonth]}</h2>
+          <h2 className="year">{currentYear}</h2>
+          <button className="buttons">
+            <i
+              className="fa-solid fa-angle-left"
+              onClick={() => {
+                setCurrentMonth((prevMon) =>
+                  prevMon === 0 ? 11 : prevMon - 1,
+                );
+                setCurrentYear((prevYear) =>
+                  currentMonth === 0 ? prevYear - 1 : prevYear,
+                );
+              }}
+            ></i>
+            <i
+              className="fa-solid fa-angle-right"
+              onClick={() => {
+                setCurrentMonth((prevMon) =>
+                  prevMon === 11 ? 1 : prevMon + 1,
+                );
+                setCurrentYear((prevYear) =>
+                  currentMonth === 11 ? prevYear + 1 : prevYear,
+                );
+              }}
+            ></i>
+          </button>
+        </div>
 
-      <div className="weakDays">
-        {daysOfWeak.map((day, index) => (
-          <span key={index}>{day}</span>
-        ))}
-      </div>
+        <div className="weakDays">
+          {daysOfWeak.map((day, index) => (
+            <span key={index}>{day}</span>
+          ))}
+        </div>
 
-      <div className="days">
-        {[...Array(firstDayInMonth).keys()].map((_, index) => (
-          <span key={`empty ${index}`}></span>
-        ))}
+        <div className="days">
+          {[...Array(firstDayInMonth).keys()].map((_, index) => (
+            <span key={`empty ${index}`}></span>
+          ))}
 
-        {[...Array(daysInMonth).keys()].map((_, index) => (
-          <span
-            key={index + 1}
-            className={
-              index + 1 === new Date().getDate() &&
-              currentMonth === new Date().getMonth() &&
-              currentYear === new Date().getFullYear()
-                ? "current-day"
-                : ""
-            }
-          >
-            {index + 1}
-          </span>
-        ))}
+          {[...Array(daysInMonth).keys()].map((_, index) => (
+            <span
+              key={index + 1}
+              className={
+                index + 1 === new Date().getDate() &&
+                currentMonth === new Date().getMonth() &&
+                currentYear === new Date().getFullYear()
+                  ? "current-day"
+                  : ""
+              }
+            >
+              {index + 1}
+            </span>
+          ))}
+        </div>
       </div>
     </div>
   );
